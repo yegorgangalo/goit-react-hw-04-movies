3811daea8eafe901de08d8dabc3c34586f7550f1
Yegor Gangalo <gangaloyegor@gmail.com>
2021-01-23 18:01:29 +0200

    try alias

--- src/views/MoviesPage/MoviesPage.js
+++ src/views/MoviesPage/MoviesPage.js
@@ -23,7 +23,6 @@ function MoviesPage() {
 
     const location = useLocation();
     const history = useHistory();
-    console.log('location', location.pathname);
 
     useEffect(() => {
     const query = new URLSearchParams(location.search).get('q');
